4b84da4cfbd11ebf6c713d99357d4170b2de896c
Karolina Bzdusek <karolina.bzdusek@gmail.com>
2021-01-07 08:06:18 +0100

    flows.apartmens

--- flows/apartments.py
+++ flows/apartments.py
@@ -5,19 +5,20 @@ def flow(parameters, datapackage, resources, stats):
 
     def apartment():
         def step(row):
-            if row['max_number'] is not None:
-                if row['WT1'] == row['max_number']:
-                    row['Category'] = 'WT1'   
-                elif row['WT2'] == row['max_number']:
-                    row['Category'] = 'WT2'
-                elif row['WT3'] == row['max_number']:
-                    row['Category'] = 'WT3'
-                elif row['WT4'] == row['max_number']:
-                    row['Category'] = 'WT4'
-                elif row['WT5'] == row['max_number']:
-                    row['Category'] = 'WT5'
-                else:
-                    row['Category'] = 'WT6'
+            if row['max_number'] == row['WT1']:
+                row['Category'] = 'WT1'   
+            elif row['max_number'] == row['WT2']:
+                row['Category'] = 'WT2'
+            elif row['max_number'] == row['WT3']:
+                row['Category'] = 'WT3'
+            elif row['max_number'] == row['WT4']:
+                row['Category'] = 'WT4'
+            elif row['max_number'] == row['WT5']:
+                row['Category'] = 'WT5'
+            elif row['max_number'] == row['WT6']:
+                row['Category'] = 'WT6'
+            else: 
+                row['Category'] = 'undefinied'
         return step
 
     return Flow(
---
+++ flows/styling.py
@@ -0,0 +1,42 @@
+from dataflows import Flow, add_field
+
+from util import *
+import math
+
+default_dict = getting_dictionary('template/default_dict.txt')
+
+def flow(parameters, datapackage, resources, stats):
+
+    def category():
+        def step(row):
+            if (row['Category'] == 'WT1'):
+                for k,v in default_dict[0].items():
+                    row[k] = v
+            if (row['Category'] == 'WT2'):
+                for k,v in default_dict[1].items():
+                    row[k] = v
+            if (row['Category'] == 'WT3'):
+                for k,v in default_dict[2].items():
+                    row[k] = v
+            if (row['Category'] == 'WT4'):
+                for k,v in default_dict[3].items():
+                    row[k] = v
+            if (row['Category'] == 'WT5'):
+                for k,v in default_dict[4].items():
+                    row[k] = v
+            else:
+                for k,v in default_dict[5].items():
+                    row[k] = v
+        return step
+
+    return Flow(
+        add_field('fill', 'string'),
+        add_field('fillColor', 'string'),
+        add_field('fillOpacity', 'string'),
+        add_field('stroke', 'string'),
+        add_field('color', 'string'),
+        add_field('opacity', 'string'),
+        add_field('weight', 'string'),
+        add_field('radius', 'number'),
+        category(),
+        )
