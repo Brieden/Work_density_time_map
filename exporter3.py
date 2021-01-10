7149f963d265e30c70d92299309431025a554ab2
Karolina Bzdusek <karolina.bzdusek@gmail.com>
2021-01-10 13:23:42 +0100

    Corrected 'reource' key

--- exporter3.py
+++ exporter3.py
@@ -2,15 +2,15 @@ import yaml
 import json
 import geopandas as gpd
 
-gdf = gpd.read_file('output/ZHR/clear_result.csv')
-gdf.to_file("output/ZHR/preview.geojson", driver='GeoJSON')
+gdf = gpd.read_file('output/ZHR3/clear_result3.csv')
+gdf.to_file("output/ZHR3/preview.geojson", driver='GeoJSON')
 # minx, miny, maxx, maxy = gdf.geometry.total_bounds
 # bbox = [minx, miny, maxx, maxy]
 # ['geo:%f,%f' % (bounds[1], bounds[0]), 'geo:%f,%f' % (bounds[3], bounds[2])]
 
-with open("output/ZHR/datapackage.json", 'r') as j, \
-     open("output/ZHR/preview.geojson", 'r') as l, \
-     open("output/ZHR/snapshot.json", 'w') as r:
+with open("output/ZHR3/datapackage.json", 'r') as j, \
+     open("output/ZHR3/preview.geojson", 'r') as l, \
+     open("output/ZHR3/snapshot.json", 'w') as r:
     data = json.load(j)
     feed = json.load(l)
     data["resources"] = [{"name": "data-layer", "mediatype": "application/vnd.simplestyle-extended", "data": {"name": "data", "type": "FeatureCollection", "features": []}},{"name": "mapbox-background", "path": "mapbox://styles/gemeindescan/ck6rp249516tg1iqkmt48o4pz", "mediatype": "application/vnd.mapbox-vector-tile", "data":"" }]
